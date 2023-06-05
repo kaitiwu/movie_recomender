bd75018bf07abed4b0239e25ca83d3788811d315
kaitiwu <hello@kaitiwu.de>
2023-06-05 11:29:09 +0200

    added workflows and other files

---
+++ utils.py
@@ -0,0 +1,16 @@
+"""
+This scripts contains 
+data and functions useful for the other scripts
+"""
+import pickle
+MOVIES = [
+    "Men in Black",
+    "Inglorious Bastards",
+    "My name is Nobody",
+    "John Wick four",
+    "Pacahontas",
+    "The Fast and Furious (8)",
+    "Tom and Jerry",
+]
+
+nmf_model = "/Users/wukaiti/Spiced/my_encounter_notes/week_10/NMF/nmf_model1.pkl"
